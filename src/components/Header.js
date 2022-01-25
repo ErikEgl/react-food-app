e6fd4d763bbdffc306784c2a5e9436e9335c8d10
Erik Egliens <erik@bellcom.ee>
2022-01-25 17:31:40 +0200

    added router + not found component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,23 @@
 import React from "react";
 
-
 const Header = (props) => (
-        <header className="top">
-          <div className="wrap">
-            <div className="header-content">
-              <div className="header-rating">
-                <div className="header-rating_tag">Rating: </div>
-                <div className="header-rating_icon">★★★★★</div>
-              </div>
-              <div className="header-divider"></div>
-              <h1 className="font-effect-fire-animation">{props.title}</h1>
-              <h3>
-                  <span>
-                      Hot <span className="sub-header">burgers</span> fast delivery 
-                  </span>
-              </h3>
-            </div>
-          </div>
-        </header>
-      );
-// class Header extends React.Component {
-//   render() {
-//     return (
-//       <header className="top">
-//         <div className="wrap">
-//           <div className="header-content">
-//             <div className="header-rating">
-//               <div className="header-rating_tag">Rating: </div>
-//               <div className="header-rating_icon">★★★★★</div>
-//             </div>
-//             <div className="header-divider"></div>
-//             <h1 className="font-effect-fire-animation">{this.props.title}</h1>
-//             <h3>
-//                 <span>
-//                     Hot <span className="sub-header">burgers</span> fast delivery 
-//                 </span>
-//             </h3>
-//           </div>
-//         </div>
-//       </header>
-//     );
-//   }
-// }
+  <header className="top">
+    <div className="wrap">
+      <div className="header-content">
+        <div className="header-rating">
+          <div className="header-rating_tag">Rating: </div>
+          <div className="header-rating_icon">★★★★★</div>
+        </div>
+        <div className="header-divider"></div>
+        <h1 className="font-effect-fire-animation">{props.title}</h1>
+        <h3>
+          <span>
+            Hot <span className="sub-header">burgers</span> fast delivery
+          </span>
+        </h3>
+      </div>
+    </div>
+  </header>
+);
 
 export default Header;
