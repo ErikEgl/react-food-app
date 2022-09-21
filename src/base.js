57404230858cc25ac968e9b97ff0e2921bf2b153
Erik Egliens <erik@bellcom.ee>
2022-09-21 15:32:05 +0300

    authDomain change

--- src/base.js
+++ src/base.js
@@ -5,7 +5,7 @@ import "firebase/auth";
 
 const firebaseApp = firebase.initializeApp({
   apiKey: "AIzaSyDE5Jw52Ys4uQlbK0i73krAr-20Ep9yweI",
-  authDomain: "burger-app-75e1d.firebaseapp.com",
+  authDomain: "https://erikegl.github.io",
   databaseURL:
     "https://burger-app-75e1d-default-rtdb.europe-west1.firebasedatabase.app",
   measurementId: "G-YGFGNDH8X0",
