9a4cdec298c01267d9beef3363c365f048ef664f
Erik Egliens <erik@bellcom.ee>
2022-09-21 14:59:28 +0300

    Updates

---
+++ precache-manifest.af878e3613a8fc2902f7bb380528e2c0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39cce684c9397d1c6b5c2831cddfe9ea",
+    "url": "/react-food-app/index.html"
+  },
+  {
+    "revision": "335a867209630bba4ed3",
+    "url": "/react-food-app/static/css/main.d9892f01.chunk.css"
+  },
+  {
+    "revision": "e38bd7365b8bab21c532",
+    "url": "/react-food-app/static/js/2.5bd760fe.chunk.js"
+  },
+  {
+    "revision": "335a867209630bba4ed3",
+    "url": "/react-food-app/static/js/main.b9b54549.chunk.js"
+  },
+  {
+    "revision": "65b9e9e467f50e192754",
+    "url": "/react-food-app/static/js/runtime-main.ff0065bf.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/react-food-app/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/react-food-app/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/react-food-app/static/media/up.551b5c6b.svg"
+  }
+]);
