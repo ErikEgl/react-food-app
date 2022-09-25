5092459600dbb967afc17ab1ff43b22ff999c7d3
Erik <Mr.erik.egliens@gmail.com>
2022-09-25 23:11:47 +0300

    sample burgers iamge src change

--- src/sample-burgers.js
+++ src/sample-burgers.js
@@ -2,7 +2,7 @@
 const sampleBurgers = {
   burger1: {
     name: 'Cheeseburger',
-    image: '/react-food-app/images/cheese.jpg',
+    image: 'https://media.istockphoto.com/photos/hamburger-with-fries-picture-id617364554?k=20&m=617364554&s=612x612&w=0&h=OJSvlDAwzSTkzpHvrK7B6DeE_99RPINIx8GGc_iwNrA=',
     desc:
       'Classic 100% beef steak burger with pickled cucumbers and fresh wheat bun.',
     price: 5.5,
@@ -11,7 +11,7 @@ const sampleBurgers = {
 
   burger2: {
     name: 'Chickenburger',
-    image: '/react-food-app/images/chicken.jpg',
+    image: 'https://media.istockphoto.com/photos/southern-country-fried-chicken-sandwich-picture-id521207406?k=20&m=521207406&s=612x612&w=0&h=H0aPzFnX97i-jzYjlXKgQXNc40qiJ-gt-0ss6a9NC0M=',
     desc:
       'Chicken burger topped with melted cheese, BBQ sauce and lettuce.',
     price: 7,
@@ -20,7 +20,7 @@ const sampleBurgers = {
 
   burger3: {
     name: 'Double Hamburger',
-    image: '/react-food-app/images/double.jpg',
+    image: 'https://media.istockphoto.com/photos/classic-burger-with-special-sauce-picture-id157586054?k=20&m=157586054&s=612x612&w=0&h=EiwbWsXQKfw7gVW9nbp2-CJ4369oIzUMmXD4VIorsn4=',
     desc:
       'Double hamburger with 100% beef steak and melted cheddar cheese and spicy sauce.',
     price: 10,
@@ -29,7 +29,7 @@ const sampleBurgers = {
 
   burger4: {
     name: 'Fishburger',
-    image: '/react-food-app/images/fish.jpg',
+    image: 'https://media.istockphoto.com/photos/beer-battered-fish-burger-picture-id533224912?k=20&m=533224912&s=612x612&w=0&h=wMTbOzxI1Ld9D22rvlE_4b0GpiG2BvJc4sObX8qbivQ=',
     desc:
       'Top quality fish fillet with leaf lettuce, pickled cucumbers and seasoned with light mayonnaise.',
     price: 7.5,
@@ -38,7 +38,7 @@ const sampleBurgers = {
 
   burger5: {
     name: 'French fries',
-    image: '/react-food-app/images/fries.jpg',
+    image: 'https://media.istockphoto.com/photos/basket-of-famous-fast-food-french-fries-picture-id618214356?k=20&m=618214356&s=612x612&w=0&h=nhxDfsZapV1XgBB7gDPDhpQEvIpjYUAWvab13vqUve8=',
     desc:
       'Crispy Golden Hot Potato Super Fry Slices Made From Selected Organic Root Vegetables!',
     price: 4.25,
@@ -47,7 +47,7 @@ const sampleBurgers = {
 
   burger6: {
     name: 'King Burger',
-    image: '/react-food-app/images/ham.jpg',
+    image: 'https://media.istockphoto.com/photos/american-cuisine-large-royal-burger-with-a-double-meat-cutlet-bacon-picture-id1195560498?k=20&m=1195560498&s=612x612&w=0&h=DXGJPQtMKXRhIzovKwO2OL1fT9pmQRv78CvlDb1rSIo=',
     desc:
       '100% selected beef burger with fried bacon slices and fresh melted cheese dressed with ketchup and mayonnaise.',
     price: 17,
@@ -56,7 +56,7 @@ const sampleBurgers = {
 
   burger7: {
     name: 'Mega Burger',
-    image: '/react-food-app/images/mega.jpg',
+    image: 'https://media.istockphoto.com/photos/tower-of-giant-tall-burger-picture-id1096613088?k=20&m=1096613088&s=612x612&w=0&h=6yU7u8ofGdb3GIeW4_zcbvUeq0re722QcTShmh0vZw4=',
     desc:
       'Our largest burger ever made with fresh steak topped with melted cheddar cheese and pickles.',
     price: 20,
@@ -65,7 +65,7 @@ const sampleBurgers = {
 
   burger8: {
     name: 'Burger in Pete',
-    image: '/react-food-app/images/pita.jpg',
+    image: 'https://assets.epicurious.com/photos/56043a59d55073c10c5b04b5/4:3/w_2000,h_1500,c_limit/365949_hires.jpg',
     desc:
       'Four 100% beef patties topped with melted Cheddar cheese with fresh leaf lettuce in a fresh wheat tortilla.',
     price: 15,
@@ -74,7 +74,7 @@ const sampleBurgers = {
 
   burger9: {
     name: 'Village Free',
-    image: '/react-food-app/images/bakedFries.jpg',
+    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpZAHU6MwHvnP6ZrewcfFt4KZAwFx3IbKVug&usqp=CAU',
     desc:
       'A portion of fresh potato slices baked in France under peacefully smoldering coals',
     price: 4.5,
